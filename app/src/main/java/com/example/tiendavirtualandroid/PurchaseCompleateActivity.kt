4ebc130715b7271e8b81397a350624aa4217df33
Luis Gustavo Cortes Castañeda <lucortes@FUNDACIONHSC.LOC>
2024-05-29 20:32:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tiendavirtualandroid/PurchaseCompleateActivity.kt
@@ -0,0 +1,21 @@
+package com.example.tiendavirtualandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import ucompensar.Desarrollo_aplicaciones_moviles.R
+
+class PurchaseCompleateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_purchase_compleate)
+
+        val btnReturnHome = findViewById<Button>(R.id.btnReturnHome)
+
+        btnReturnHome.setOnClickListener {
+            val intent = Intent(this, ProductListActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
